d72d65b2dee3d378d326b9c9304e51dd68f46d91
aashim0821 <aashimahamed0807@gmail.com>
2026-07-10 23:05:53 +0530

    Error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import Lenis from 'lenis';
 import { Scene } from './components/Canvas/Scene';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { LandingPage } from './pages/LandingPage';
@@ -74,7 +74,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <Scene />
       <div style={{ position: 'relative', zIndex: 1 }}>
@@ -88,7 +88,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
